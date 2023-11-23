cac140dcabe7f428c2659f92a396e176baee0a30
SaravananK19 <149501627+SaravananK19@users.noreply.github.com>
2023-11-23 16:55:22 +0530

    Update tscript.js

--- tscript.js
+++ tscript.js
@@ -1,19 +1,19 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
+//smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => { //select all <a> whose href starts with #,for each loop iterate over sel anchor.
+    anchor.addEventListener('click', function (e) { //add a mouse click event
+        e.preventDefault(); // prevent default beh of the anchor link, which is navigating to another page
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ //find the target element using the 'href' att of the clicked anchor
             behavior: 'smooth'
         });
     });
   });
   
   // Form submission handling
-  document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    // You can add code here to handle form submission, such as sending an email or storing the data.
-    // For demonstration purposes, let's log the form data to the console.
+  document.getElementById('contact-form').addEventListener('submit', function (e) { // add submit event listener to the contact form
+    e.preventDefault();  // after submitting, it reload the page.
+    // create new form data objects
     const formData = new FormData(this);
-    console.log('Form data:', Object.fromEntries(formData));
+    console.log('Form data:', Object.fromEntries(formData)); // save the data in console in keys:value
     // Clear the form inputs
     this.reset();
   });
